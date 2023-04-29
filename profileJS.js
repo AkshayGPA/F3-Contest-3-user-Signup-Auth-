454444ea1e119866bfe9d50b68a5d6a09a3475d8
akshay_gaikwad <akshay240695@gmail.com>
2023-04-29 23:08:12 +0530

    Project complete

--- profileJS.js
+++ profileJS.js
@@ -1,6 +1,6 @@
 // store the hyperlink for signup and profile page
-const signUpPageEl = document.getElementById('signUp');
-const profilePageEl = document.getElementById('profile');
+const signUpPageNav = document.getElementById('signUp');
+const profilePageNav = document.getElementById('profile');
 
 // store the profile details elements
 const nameEl = document.getElementById('disp-name');
@@ -16,33 +16,29 @@ userDetails.classList.remove('hidden');
 
 // ***************************************************** //
 
-// Event listeners & functions
-// 01. logged out function
+
 function logOut() {
-  // logged out means deleting the data of the last signed up user
   localStorage.removeItem('users');
   localStorage.removeItem('accessToken');
   localStorage.removeItem('state');
   userDetails.classList.add('hidden');
-  // redirect to signup page
+
   location.href = './index.html';
 }
 
-// 02. display user details
+
 function displaydetails(currUser) {
-  // currUser is an object containing all details of the last logged in/ signed up user
   nameEl.textContent = currUser.name.toUpperCase();
   emailEl.textContent = currUser.email;
   passwordEl.textContent = currUser.password;
 }
 
-// 03. Sign up page button(in navbar) event listener
-signUpPageEl.addEventListener('click', () => {
-  alert('You are already signed up!!');
+
+signUpPageNav.addEventListener('click', () => {
+  alert('You have already signed up!!');
 })
 
-// 04. Profile page button(in navbar) event listener
-profilePageEl.addEventListener('click', () => {
-  // nothing should happen
-  alert('You are present on profile page');
+
+profilePageNav.addEventListener('click', () => {
+  alert('You are currently on the profile page');
 })
